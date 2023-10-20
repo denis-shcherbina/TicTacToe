1a8028a2b4dda305374ab1515498c4b45feeb19d
Denys Shcherbina <denis.scherbina.891136@gmail.com>
2023-10-20 10:00:47 +0300

    Init commit

---
+++ app/src/main/java/com/example/tictactoe/MyServices.java
@@ -0,0 +1,7 @@
+package com.example.tictactoe;
+
+public class MyServices
+{
+    public static boolean VIBRATION_CHECK=false;
+    public static boolean SOUND_CHECK=true;
+}
---
+++ app/src/main/java/com/example/tictactoe/SettingsActivity.java
@@ -0,0 +1,89 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    Switch vibrationSwitch;
+    Switch soundSwitch;
+    private ImageView backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_settings);
+
+        vibrationSwitch = (Switch)  findViewById(R.id.vibration_switch);
+        soundSwitch = (Switch)  findViewById(R.id.sound_switch);
+        backBtn = (ImageView) findViewById(R.id.settings_back_btn);
+
+        if(MyServices.VIBRATION_CHECK)
+        {
+            vibrationSwitch.setChecked(true);
+        }
+        else if(!MyServices.VIBRATION_CHECK)
+        {
+            vibrationSwitch.setChecked(false);
+        }
+
+        vibrationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                    MyServices.VIBRATION_CHECK =true;
+                }
+                else {
+                    MyServices.VIBRATION_CHECK= false;
+                }
+            }
+        });
+
+        if(MyServices.SOUND_CHECK)
+        {
+            soundSwitch.setChecked(true);
+        }
+        else if(!MyServices.SOUND_CHECK)
+        {
+            soundSwitch.setChecked(false);
+        }
+
+        soundSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                    MyServices.SOUND_CHECK =true;
+                }
+                else {
+                    MyServices.SOUND_CHECK= false;
+                }
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
